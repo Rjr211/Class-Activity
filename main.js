97976ba512fe4205a8f032047f4f53d156c52a7c
Edward Nunez <edwardnnz@gmail.com>
2018-06-16 11:18:44 -0400

    firebase submit eventlistener for form

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// Initialize Firebase
-var config = {
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyBOk-QMHgH8gyO-9xdFP-JVJGF9NR2vVJA",
     authDomain: "test-591bb.firebaseapp.com",
     databaseURL: "https://test-591bb.firebaseio.com",
     projectId: "test-591bb",
     storageBucket: "test-591bb.appspot.com",
     messagingSenderId: "115420572333"
-};
+  };
+  firebase.initializeApp(config);
 
-firebase.initializeApp(config);
 
 //firebase database variable
 var database = firebase.database();
@@ -16,22 +16,30 @@ var database = firebase.database();
 var employeeName = "",
     role = "",
     startDate = "",
-    monthlyRate = "",
+    monthlyRate = "";
 
-function monthsWorked (startDate) {
+//function monthsWorked (startDate) {
 
-};
+//};
 
-function totalBilled (monthlyRate, monthsWorked) {
-   var totalBilled = monthlyRate * monthsWorked;
-   return totalBilled;
-}
+// function totalBilled (monthlyRate, monthsWorked) {
+//    var totalBilled = monthlyRate * monthsWorked;
+//    return totalBilled;
+// }
 
-$("form").submit(function(event) {
+$("#target").submit(function(event) {
     event.preventDefault();
 
-    database.ref("").push({
+    employeeName = $("#employee-name-input").val().trim();
+    role = $("#role-input").val().trim();
+    startDate = $("#start-input").val().trim();
+    monthlyRate = $("#rate-input").val().trim();
 
+    database.ref("/employeeData").push({
+        "employeeName": employeeName,
+        "role": role,
+        "startDate": startDate,
+        "monthlyRate": monthlyRate,
     })
 
 });
